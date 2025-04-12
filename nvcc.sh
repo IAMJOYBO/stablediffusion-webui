2cf88770a65e6104e18e8797bd1d4aa71026db06
IAMJOYBO <160575243+IAMJOYBO@users.noreply.github.com>
2025-04-12 13:18:18 +0800

    Update nvcc.sh

--- nvcc.sh
+++ nvcc.sh
@@ -1,6 +1,6 @@
-wget https://developer.download.nvidia.com/compute/cudnn/9.8.0/local_installers/cudnn-local-repo-ubuntu2004-9.8.0_1.0-1_amd64.deb
-dpkg -i cudnn-local-repo-ubuntu2004-9.8.0_1.0-1_amd64.deb
-cp /var/cudnn-local-repo-ubuntu2004-9.8.0/cudnn-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cudnn/9.8.0/local_installers/cudnn-local-repo-ubuntu2204-9.8.0_1.0-1_amd64.deb
+dpkg -i cudnn-local-repo-ubuntu2204-9.8.0_1.0-1_amd64.deb
+cp -rf /var/cudnn-local-repo-ubuntu2204-9.8.0/cudnn-*-keyring.gpg /usr/share/keyrings/
 apt-get update
 apt-get -y install cudnn
 apt-get -y install cudnn-cuda-12
